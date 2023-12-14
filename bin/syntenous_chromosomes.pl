4cc4a61ba5d0fe04e2a55f8583d5504d4f7e73d2
chriswyatt1 <cw13722@gmail.com>
2023-12-14 13:06:42 +0000

    Plot_version_2_testing

--- bin/syntenous_chromosomes.pl
+++ bin/syntenous_chromosomes.pl
@@ -3,7 +3,7 @@
 use warnings;
 use strict;
 
-die "Provide the name of the Chromopaint.txt file\n" if (@ARGV!=3); 
+die "Provide the name of the first bed, second bed, anchor (.new) file file\n" if (@ARGV!=3); 
 
 print "Script is running\n";
 
@@ -22,7 +22,7 @@ my %SP1;
 while (my $line=<$BED1>) {
     chomp $line;
 	my @spl=split ("\t", $line);
-    $SP1{$spl[0]}={$spl[3]};
+    $SP1{$spl[3]}=$spl[0];
 }
 
 #read in the secon bed file
@@ -30,7 +30,8 @@ my %SP2;
 while (my $line2=<$BED2>) {
     chomp $line2;
 	my @spl=split ("\t", $line2);
-    $SP2{$spl[0]}={$spl[3]};
+    #print "$spl[0] and  $spl[3]\n";
+    $SP2{$spl[3]}=$spl[0];
 }
 
 #Calc the scaffold name in the anchors file
@@ -41,47 +42,44 @@ while (my $line3=<$ANCH>) {
     my @spl=split ("\t", $line3);
     my $len=scalar(@spl);
     if ( $len == 3 ){
-        my $chr1=$SP1{$spl[0]};
-        my $chr2=$SP2{$spl[1]};
-        my $join="$chr1\_$chr2";
-        $chromo_pairs{"$join"}="hit";
+        my $chr1=$SP1{"$spl[0]"};
+        my $chr2=$SP2{"$spl[1]"};
+
+        #print "$line3\nch1 $chr1 ch2 $chr2  do they exixsts, and if not, why not\n";
+        my $join="$chr1\_\@\_$chr2";
+        $chromo_pairs{"$join"}++;
     }
     else{
         #probably a # start line between syntenic regions.
     }
 }
 
-=cut
+my %done_check_sp1;
+my %done_check_sp2;
+my @final_list_sp1;
+my @final_list_sp2;
+foreach my $key ( sort {$chromo_pairs{$b} <=> $chromo_pairs{$a}} keys %chromo_pairs ) {
 
-foreach my $chrome_sp1 ( keys %SP1) {
-    my $best_score=0;
-    foreach my $chrome_sp2 ( keys % { $SP1{$chrome_sp1} } ) {
-        my $score = $SP1{$chrome_sp1}{$chrome_sp2};
-        if ($score >= $best_score){
-            $best_chromo{$chrome_sp1}=$chrome_sp2;
-            $best_score=$score;
-            #print "$chrome_sp1 to $chrome_sp2  score $best_score\n";
-        }
-    }
-}
-
-#Work out new chromo name based on the previous data.
-my %done;
-my %Final_names;
-foreach my $chrome_sp1 ( keys %best_chromo) {
-    if ($done{$chrome_sp1}){
-        $Final_names{$chrome_sp1}="$best_chromo{$chrome_sp1}\_$done{$chrome_sp1}";
-        $done{$chrome_sp1}++;
+    print "$key $chromo_pairs{$key}\n";
+    my @spl=split("\_\@\_", $key);
+    if ($done_check_sp1{$spl[0]}){
+        #do nothing
     }
     else{
-        $done{$chrome_sp1}=2;
-        $Final_names{$chrome_sp1}=$best_chromo{$chrome_sp1};
+        push (@final_list_sp1, "$spl[0]");
+        $done_check_sp1{$spl[0]}="yes"
+    }
+
+    if ($done_check_sp2{$spl[1]}){
+        #do nothing
+    }
+    else{
+        push (@final_list_sp2, "$spl[1]");
+        $done_check_sp2{$spl[1]}="yes"
     }
 }
 
+my $join_sp1=join("\,", @final_list_sp1);
+my $join_sp2=join("\,", @final_list_sp2);
 
-#finally print of the new chromo name hash.
-
-foreach my $sp1 ( keys %Final_names) {
-    print $out "$sp1\t$Final_names{$sp1}\n";
-}
+print $out "$join_sp1\n$join_sp2\n";
--- modules/synteny.nf
+++ modules/synteny.nf
@@ -4,6 +4,7 @@ process SYNTENY {
     publishDir "$params.outdir/Jcvi_results" , mode: "copy", pattern: "*.anchors"
     publishDir "$params.outdir/Jcvi_results" , mode: "copy", pattern: "*.pdf"
     publishDir "$params.outdir/Jcvi_results" , mode: "copy", pattern: "*.percent.similarity"
+    publishDir "$params.outdir/Karyotype_results" , mode: "copy", pattern: "*.karyotype.pdf"
     publishDir "$params.outdir/Last" , mode: "copy", pattern: "*last.filtered"
     container = 'chriswyatt/jcvi'
     errorStrategy = 'ignore'    
@@ -24,9 +25,11 @@ process SYNTENY {
         python -m jcvi.compara.catalog ortholog ${sample_id} ${sample_id2} --no_strip_names
         python -m jcvi.compara.synteny depth --histogram ${sample_id}.${sample_id2}.anchors
         cut -f 3 ${sample_id}.${sample_id2}.last.filtered  | awk '{ sum += \$1; n++ } END { if (n > 0) print sum / n; }' > ${sample_id}.${sample_id2}.percent.similarity
-        python -m jcvi.compara.synteny screen --minspan=30 --simple ${sample_id} ${sample_id2}.anchors ${sample_id} ${sample_id2}.anchors.new 
-        echo "# y, xstart, xend, rotation, color, label, va,  bed\n.6,     .1,    .8,       0,      , ${sample_id}, top, ${sample_id}.bed\n.4,     .1,    .8,       0,      , ${sample_id2}, top, ${sample_id2}.bed\n# edges\ne, 0, 1, ${sample_id}.${sample_id2}.anchors.simple" > layout_default        
-
+        python -m jcvi.compara.synteny screen --minspan=30 --simple ${sample_id}.${sample_id2}.anchors ${sample_id}.${sample_id2}.anchors.new 
+        echo "# y, xstart, xend, rotation, color, label, va,  bed\n.6,     .1,    .8,       0,      , ${sample_id}, top, ${sample_id}.bed\n.4,     .1,    .8,       0,      , ${sample_id2}, top, ${sample_id2}.bed\n# edges\ne, 0, 1, ${sample_id}.${sample_id2}.anchors.simple" > layout      
+        syntenous_chromosomes.pl ${sample_id}.bed ${sample_id2}.bed ${sample_id}.${sample_id2}.anchors.new
+        python -m jcvi.graphics.karyotype seqids_karyotype.txt layout
+        mv karyotype.pdf ${sample_id}.${sample_id2}\.karyotype.pdf
     """
 }
 
