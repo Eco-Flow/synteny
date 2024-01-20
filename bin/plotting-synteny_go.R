722254d642ef8237dd0f44cb49d03897cfbc695d
Christopher Wyatt <cw13722@gmail.com>
2024-01-20 18:20:40 +0000

    Summary_go_R_script_fixed

--- bin/plotting-synteny_go.R
+++ bin/plotting-synteny_go.R
@@ -6,10 +6,17 @@ rownames(newdata) <- paste(newdata$GO_ID,newdata$GO_term)
 df = subset(newdata, select = -c(GO_term,GO_ID,Count_significant) )
 df[is.na(df)] <- 1
 df2<-as.matrix(df)
-my_palette <- colorRampPalette(c("purple","red", "orange", "yellow", "white")) (n=20)
-pdf("Go_summary_topSynteny.pdf", width=6, height=5)
-pheatmap::pheatmap(log10(head(df2, n=30)), col=my_palette, cluster_rows = F, treeheight_row = 0, treeheight_col = 0, legend=T)
-dev.off()
+df3<-as.data.frame(df2)
+my_mean<-mean(df2)
+if (my_mean == 1){
+	#Do nothing, no results were significant, so plotting will fail
+} else {
+	my_palette <- colorRampPalette(c("purple","red", "orange", "yellow", "white")) (n=20)
+	pdf("Go_summary_topSynteny.pdf", width=6, height=5)
+	pheatmap::pheatmap(log10(head(df2, n=30)), col=my_palette, cluster_rows = F, treeheight_row = 0, treeheight_col = 0, legend=T)
+	dev.off()
+}
+
 
 erefd<-read.table("Go_summary_botSynteny.tsv", h=T, sep="\t")
 newdata <- erefd[order(erefd$Count_significant, decreasing = T),]
@@ -17,10 +24,15 @@ rownames(newdata) <- paste(newdata$GO_ID,newdata$GO_term)
 df = subset(newdata, select = -c(GO_term,GO_ID,Count_significant) )
 df[is.na(df)] <- 1
 df2<-as.matrix(df)
-my_palette <- colorRampPalette(c("purple","red", "orange", "yellow", "white")) (n=20)
-pdf("Go_summary_botSynteny.pdf", width=6, height=5)
-pheatmap::pheatmap(log10(head(df2, n=30)), col=my_palette, cluster_rows = F, treeheight_row = 0, treeheight_col = 0, legend=T)
-dev.off()
+my_mean<-mean(df2)
+if (my_mean == 1){
+	#Do nothing, no results were significant, so plotting will fail
+} else {
+	my_palette <- colorRampPalette(c("purple","red", "orange", "yellow", "white")) (n=20)
+	pdf("Go_summary_botSynteny.pdf", width=6, height=5)
+	pheatmap::pheatmap(log10(head(df2, n=30)), col=my_palette, cluster_rows = F, treeheight_row = 0, treeheight_col = 0, legend=T)
+	dev.off()
+}
 
 
 erefd<-read.table("Go_summary_averhigh.tsv", h=T, sep="\t")
@@ -29,10 +41,15 @@ rownames(newdata) <- paste(newdata$GO_ID,newdata$GO_term)
 df = subset(newdata, select = -c(GO_term,GO_ID,Count_significant) )
 df[is.na(df)] <- 1
 df2<-as.matrix(df)
-my_palette <- colorRampPalette(c("purple","red", "orange", "yellow", "white")) (n=20)
-pdf("Go_summary_averhigh.pdf", width=6, height=5)
-pheatmap::pheatmap(log10(head(df2, n=30)), col=my_palette, cluster_rows = F, treeheight_row = 0, treeheight_col = 0, legend=T)
-dev.off()
+my_mean<-mean(df2)
+if (my_mean == 1){
+	#Do nothing, no results were significant, so plotting will fail
+} else {
+	my_palette <- colorRampPalette(c("purple","red", "orange", "yellow", "white")) (n=20)
+	pdf("Go_summary_averhigh.pdf", width=6, height=5)
+	pheatmap::pheatmap(log10(head(df2, n=30)), col=my_palette, cluster_rows = F, treeheight_row = 0, treeheight_col = 0, legend=T)
+	dev.off()
+}
 
 erefd<-read.table("Go_summary_averlow.tsv", h=T, sep="\t")
 newdata <- erefd[order(erefd$Count_significant, decreasing = T),]
@@ -40,10 +57,15 @@ rownames(newdata) <- paste(newdata$GO_ID,newdata$GO_term)
 df = subset(newdata, select = -c(GO_term,GO_ID,Count_significant) )
 df[is.na(df)] <- 1
 df2<-as.matrix(df)
-my_palette <- colorRampPalette(c("purple","red", "orange", "yellow", "white")) (n=20)
-pdf("Go_summary_averlow.pdf", width=6, height=5)
-pheatmap::pheatmap(log10(head(df2, n=30)), col=my_palette, cluster_rows = F, treeheight_row = 0, treeheight_col = 0, legend=T)
-dev.off()
+my_mean<-mean(df2)
+if (my_mean == 1){
+	#Do nothing, no results were significant, so plotting will fail
+} else {
+	my_palette <- colorRampPalette(c("purple","red", "orange", "yellow", "white")) (n=20)
+	pdf("Go_summary_averlow.pdf", width=6, height=5)
+	pheatmap::pheatmap(log10(head(df2, n=30)), col=my_palette, cluster_rows = F, treeheight_row = 0, treeheight_col = 0, legend=T)
+	dev.off()
+}
 
 erefd<-read.table("Go_summary_highScore.tsv", h=T, sep="\t")
 newdata <- erefd[order(erefd$Count_significant, decreasing = T),]
@@ -51,10 +73,15 @@ rownames(newdata) <- paste(newdata$GO_ID,newdata$GO_term)
 df = subset(newdata, select = -c(GO_term,GO_ID,Count_significant) )
 df[is.na(df)] <- 1
 df2<-as.matrix(df)
-my_palette <- colorRampPalette(c("purple","red", "orange", "yellow", "white")) (n=20)
-pdf("Go_summary_highScore.pdf", width=6, height=5)
-pheatmap::pheatmap(log10(head(df2, n=30)), col=my_palette, cluster_rows = F, treeheight_row = 0, treeheight_col = 0, legend=T)
-dev.off()
+my_mean<-mean(df2)
+if (my_mean == 1){
+	#Do nothing, no results were significant, so plotting will fail
+} else {
+	my_palette <- colorRampPalette(c("purple","red", "orange", "yellow", "white")) (n=20)
+	pdf("Go_summary_highScore.pdf", width=6, height=5)
+	pheatmap::pheatmap(log10(head(df2, n=30)), col=my_palette, cluster_rows = F, treeheight_row = 0, treeheight_col = 0, legend=T)
+	dev.off()
+}
 
 erefd<-read.table("Go_summary_lowScore.tsv", h=T, sep="\t")
 newdata <- erefd[order(erefd$Count_significant, decreasing = T),]
@@ -62,9 +89,14 @@ rownames(newdata) <- paste(newdata$GO_ID,newdata$GO_term)
 df = subset(newdata, select = -c(GO_term,GO_ID,Count_significant) )
 df[is.na(df)] <- 1
 df2<-as.matrix(df)
-my_palette <- colorRampPalette(c("purple","red", "orange", "yellow", "white")) (n=20)
-pdf("Go_summary_lowScore.pdf", width=6, height=5)
-pheatmap::pheatmap(log10(head(df2, n=30)), col=my_palette, cluster_rows = F, treeheight_row = 0, treeheight_col = 0, legend=T)
-dev.off()
+my_mean<-mean(df2)
+if (my_mean == 1){
+	#Do nothing, no results were significant, so plotting will fail
+} else {
+	my_palette <- colorRampPalette(c("purple","red", "orange", "yellow", "white")) (n=20)
+	pdf("Go_summary_lowScore.pdf", width=6, height=5)
+	pheatmap::pheatmap(log10(head(df2, n=30)), col=my_palette, cluster_rows = F, treeheight_row = 0, treeheight_col = 0, legend=T)
+	dev.off()
+}
 
 
